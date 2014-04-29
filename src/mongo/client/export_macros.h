acab45050d24a7d0ea681141137a2c7a37e51725
Andrew Morrow <acm@mongodb.com>
2014-04-29 19:27:55 -0400

    More cmake hacking

--- src/mongo/client/export_macros.h
+++ src/mongo/client/export_macros.h
@@ -15,41 +15,8 @@
 
 #pragma once
 
+#include "mongo/export.h"
+
 #include "mongo/platform/compiler.h"
 
 #define MONGO_CLIENT_FUNC MONGO_COMPILER_API_CALLING_CONVENTION
-
-/**
- * Definition of macros used to label the mongo client api.
- *
- * If a type, free function or global variable is part of the client api, it must be labeled.
- *
- * To label a type, place the MONGO_CLIENT_API macro after the struct, class or enum keyword.
- * Example:
- *   class MONGO_CLIENT_API DBClientInterface { ... };
- *
- * To label a function, place the label on the declaration before the return type.  You
- * do NOT need to label the methods of exported classes.
- * Example:
- *   MONGO_CLIENT_API Status myFreeFunction(int arg1);
- *
- * To label a global variable, place the label on the declaration, before the type and
- * after the "extern" keyword.
- * Example:
- *   extern MONGO_CLIENT_API int myGlobalVariable;
- *
- */
-
-#if defined(STATIC_LIBMONGOCLIENT)
-
-#define MONGO_CLIENT_API
-
-#else
-
-#if defined(LIBMONGOCLIENT_BUILDING)
-#define MONGO_CLIENT_API MONGO_COMPILER_API_EXPORT
-#else
-#define MONGO_CLIENT_API MONGO_COMPILER_API_IMPORT
-#endif
-
-#endif
